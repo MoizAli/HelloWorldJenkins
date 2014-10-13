3ce2f97827e71e13b7b63ff7bf4a178c5259db26
Moiz Ali <moiz.zuberi@shopnow.com>
2014-10-13 11:41:52 +0200

    adding hello world jenkins

---
+++ app/src/main/java/com/example/mzuberi1/helloworld/RendererExample.java
@@ -0,0 +1,33 @@
+package com.example.mzuberi1.helloworld;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pedrogomez.renderers.Renderer;
+
+/**
+ * Created by mzuberi1 on 23/09/14.
+ */
+public class RendererExample extends Renderer<Video> {
+
+    @Override
+    protected void setUpView(View rootView) {
+
+    }
+
+    @Override
+    protected void hookListeners(View rootView) {
+
+    }
+
+    @Override
+    protected View inflate(LayoutInflater inflater, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
---
+++ app/src/main/java/com/example/mzuberi1/helloworld/Video.java
@@ -0,0 +1,16 @@
+package com.example.mzuberi1.helloworld;
+
+/**
+ * Created by mzuberi1 on 23/09/14.
+ */
+public class Video {
+    private String mUrl;
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
